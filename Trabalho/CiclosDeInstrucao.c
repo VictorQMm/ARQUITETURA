ea2cc088ce68eb8ea438cbe9547ffc27c1802e6b
Victor Querino Martins <123746753+VictorQMm@users.noreply.github.com>
2024-10-09 00:13:19 -0300

    att

--- Trabalho/CiclosDeInstrucao.c
+++ Trabalho/CiclosDeInstrucao.c
@@ -4,17 +4,17 @@
 
 typedef enum {
     Instru000001 = 1,
-    Instru000010 = 2,
-    Instru000011 = 3,
-    Instru000100 = 4,
-    Instru000101 = 5,
-    Instru000110 = 6,
-    Instru000111 = 7,
-    Instru001000 = 8,
-    Instru001010 = 9,
-    Instru001011 = 10,
-    Instru001111 = 11,
-    Instru001100 = 12
+    Instru000010,
+    Instru000011,
+    Instru000100,
+    Instru000101,
+    Instru000110,
+    Instru000111,
+    Instru001000,
+    Instru001010,
+    Instru001011,
+    Instru001111,
+    Instru001100
 } TipoInstrucao;
 
 typedef struct {
@@ -51,7 +51,7 @@ void iniciar() {
     char instrucao[10];
     int operando1, operando2;
 
-    printf("Insira as instrucoees do programa. Digite 'END' para finalizar.\n");
+    printf("Insira as instrucoes do programa. Digite '001100' para finalizar.\n");
 
     while (1) {
         printf("Instrucao: ");
@@ -65,7 +65,6 @@ void iniciar() {
         printf("Operando2: ");
         scanf("%d", &operando2);
         
-
         instrucaoo[numInstru].tipo = tipo;
         instrucaoo[numInstru].operando1 = operando1;
         instrucaoo[numInstru].operando2 = operando2;
@@ -77,60 +76,69 @@ void executar() {
     while (CP < numInstru) { 
         Instrucao instr = instrucaoo[CP]; 
         switch (instr.tipo) { 
-            case Instru000001:
-            printf("CP: %d\n", CP);
+            case Instru000001: // Carregar memória no MBR
+                MBR = memoria[instr.operando1]; 
+                printf("MBR = %d, CP = %d\n", MBR, CP);
                 break; 
-            case Instru000010:
-                MBR = memoria[instr.operando1]; // Carrega o valor em MBR
-                ACC = MBR; //  MBR -> acumulador
-                printf(" ACC = %d, MBR = %d, CP = %d\n", ACC, MBR, CP);
+            case Instru000010: // Armazenar valor na memória
+                memoria[instr.operando1] = instr.operando2;
+                printf("Memoria[%d] = %d, CP = %d\n", instr.operando1, instr.operando2, CP);
                 break;
-            case Instru000011:
-                MBR = ACC; // Acumulador -> MBR
-                memoria[instr.operando1] = MBR; // MBR -> memória
+            case Instru000011: // Adicionar valor da memória no MBR
+                MBR += memoria[instr.operando1];
+                printf("MBR = %d, CP = %d\n", MBR, CP);
+                break;
+            case Instru000100: // Carregar memória no ACC
+                MBR = memoria[instr.operando1];
+                ACC = MBR;
                 printf("ACC = %d, MBR = %d, CP = %d\n", ACC, MBR, CP);
                 break;
-            case Instru000100:
-                MBR = memoria[instr.operando1]; // Carrega  memória no MBR
-                ACC += MBR; // MBR -> acumulador
+            case Instru000101: // Subtrai memória do ACC
+                MBR = memoria[instr.operando1];
+                ACC -= MBR;
                 printf("ACC = %d, MBR = %d, CP = %d\n", ACC, MBR, CP);
                 break;
-            case Instru000101:
-                MBR = memoria[instr.operando1]; // Carrega memória no MBR
-                ACC -= MBR; 
-                printf("ACC = %d, MBR = %d, CP = %d\n", ACC, MBR, CP);
-                break;
-            case Instru000110:
-                CP = instr.operando1 - 1; //ajusta o contador
+            case Instru000110: // Endereço especifico
+                CP = instr.operando1 - 1;
                 printf("CP = %d\n", CP);
                 break;
-            case Instru000111:
-                if (ACC == 0) CP = instr.operando1 - 1; // Salta se o acumulador for zero
+            case Instru000111: // ACC se = a zero
+                if (ACC == 0) CP = instr.operando1 - 1;
                 printf("ACC = %d, CP = %d\n", ACC, CP);
                 break;
-            case Instru001000:
-                if (ACC != 0) CP = instr.operando1 - 1; // Salta se o acumulador não for zero
+            case Instru001000: // ACC se diferente de zero
+                if (ACC != 0) CP = instr.operando1 - 1;
                 printf("ACC = %d, CP = %d\n", ACC, CP);
                 break;
             case Instru001010: 
                 MBR = (int)sqrt(MBR);
                 printf("MBR = %d, CP = %d\n", MBR, CP);
                 break;
-            case Instru001011: 
+            case Instru001011: //  MBR - MBR
                 MBR = -MBR;
                 printf("MBR = %d, CP = %d\n", MBR, CP);
                 break;
-            case Instru001111: 
-                memoria[instr.operando1] = MBR; //MBR -> posição especificada da memória
-                  printf("MBR = %d, Memoria[%d] = %d, CP = %d\n", MBR, instr.operando1, memoria[instr.operando1], CP);
+            case Instru001111: //  MBR -> memory
+                memoria[instr.operando1] = MBR;
+                printf("Memoria[%d] = %d, CP = %d\n", instr.operando1, MBR, CP);
                 break;
             case Instru001100: 
-            printf(" CP: %d\n", CP);
+                printf("Fim, CP = %d\n", CP);
+                return; 
+            default:
+                printf("Instrucao invalida CP: %d\n", CP);
                 break;
         }
         CP++; 
 
         printf("Valor final do acumulador: %d\n\n", ACC);
+    }
+
+    printf("Estado final da memoria:\n");
+    for (int i = 0; i < 256; i++) {
+        if (memoria[i] != 0) {
+            printf("Memoria[%d] = %d\n", i, memoria[i]);
+        }
     }
 }
 
